607cf9fe6b89dca9f5b0a4c347ce5ae3ab9a9993
Gnasp <GnaspGames@users.noreply.github.com>
2016-08-29 22:31:53 +0100

    Test pre-release version

--- release.sh
+++ release.sh
@@ -4,10 +4,21 @@ echo Making sure files use correct line endings
 
 dos2unix *.js
 
-echo Pushing to Github
-
-git push origin master:master
-
 echo Publish via npm
 
-npm publish
+echo "What type of release is this?"
+
+select MODE in "Pre-release" "Stable"; do
+  break
+done
+
+if [[ ! $MODE ]]; then
+	echo "Incorrect release type"
+	exit 1
+fi
+
+if [ "$MODE" = "Pre-release" ]; then
+	npm publish --tag=pre
+elif [ "$MODE" = "Stable" ]; then
+	npm publish
+fi
