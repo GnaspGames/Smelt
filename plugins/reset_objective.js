91e63281bff38485e1d53041a0efa5681a5215c5
Gnasp <GnaspGames@users.noreply.github.com>
2016-08-29 18:49:07 +0100

    Fix: !reset_objective is broken
    This fixes #61

--- plugins/reset_objective.js
+++ plugins/reset_objective.js
@@ -14,8 +14,8 @@ ResetObjective.Execute = function(smelt)
 {
 	var name = smelt.args[0];
 	var criteria = smelt.args[1];
-	args.shift();
-	args.shift();
+	smelt.args.shift();
+	smelt.args.shift();
 	var displayName = smelt.args.join(" ");
 	if(name)
 	{
