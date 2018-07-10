7c505ae352536ea9aca320af06d2bf101a23f0b3
nift4 <36458596+nift4@users.noreply.github.com>
2018-07-10 14:28:28 +0200

    Smelt to Molten

---
+++ Compiler/CommandTemplates.js
@@ -0,0 +1,35 @@
+var Settings = require("../Configuration/Settings");
+
+var Templates = 
+{
+	Current : null,
+    MC_1_13: {
+        IMPULSE_BLOCK_NAME : "command_block",
+		REPEATING_BLOCK_NAME : "repeating_command_block",
+		CHAIN_BLOCK_NAME : "chain_command_block",
+		TESTFORBLOCK_COMMAND_FORMAT: "testforblock ~%d ~%d ~%d minecraft:%s -1 {SuccessCount:0}",
+		SETBLOCK_COMMAND_FORMAT : "setblock ~%d ~%d ~%d %s[facing=%s,conditional=%s]{Command:%s%s%s} replace",
+		SUMMON_ARMORSTAND_DISPLAY_MARKER_FORMAT : "summon minecraft:armor_stand ~%d ~%d ~%d {CustomName:\"{\\\"text\\\":\\\"%s\\\"}\", Tags:[\"oc_marker\"], Marker:1b, CustomNameVisible:1b, Invulnerable:1b, NoGravity:1b, Invisible:1b}",
+		SUMMON_ARMORSTAND_CMD_MARKER_FORMAT : "summon minecraft:armor_stand ~%d ~%d ~%d {Tags:[\"oc_marker\",\"%s\"], Marker:1b, Invulnerable:1b, NoGravity:1b}",	
+		SUMMON_AEC_DISPLAY_MARKER_FORMAT : "summon minecraft:area_effect_cloud ~%d ~%d ~%d {CustomName:{\"{\\\"text\\\":\\\"%s\\\"}\", CustomNameVisible:1b, Tags:[\"oc_marker\"], Duration:2147483647}",
+		SUMMON_AEC_CMD_MARKER_FORMAT : "summon minecraft:area_effect_cloud ~%d ~%d ~%d {Tags:[\"oc_marker\",\"%s\"], Duration:2147483647}",
+		SUMMON_REBUILD_ENTITY : "summon minecraft:armor_stand ~%d ~%d ~%d {Tags:[\"oc_rebuild\"]}",
+		CLEAR_AREA_FORMAT: "execute as @e[tag=oc_rebuild] run fill ~%d ~%d ~%d ~%d ~%d ~%d minecraft:air hollow",
+		CLEAR_MARKERS_FORMAT: "execute as @e[tag=oc_rebuild] run kill @e[tag=oc_marker,dx=%d,dy=%d,dz=%d]",
+		CLEAR_MODULE_DISPLAY_MARKER: "kill @e[tag=oc_marker,distance=0]",
+		CLEAR_REBUILD_ENTITY: "kill @e[tag=oc_rebuild]",
+		CLEAR_MINECARTS: "kill @e[type=command_block_minecart,distance=0]",
+		COMMAND_BLOCK_MINECART_NBT_FORMAT: "{Time:1,id:command_block_minecart,BlockState:{Name:command_block},Command:%s}",
+		SUMMON_FALLING_RAIL_FORMAT: "summon minecraft:falling_block ~ ~5 ~ {BlockState:{Name:activator_rail},Time:1,Passengers:[%s]}"
+    }
+}
+
+var getCurrent = function()
+{
+	var current = Templates.MC_1_13;
+	return current;
+}
+
+Templates.Current = getCurrent();
+
+module.exports = Templates;
---
+++ Compiler/FileParser.js
@@ -0,0 +1,444 @@
+var util = require('util');
+var fs = require('fs');
+var path = require('path');
+var chalk = require('chalk');
+var ncp = require("copy-paste");
+
+var Templates = require("./CommandTemplates");
+var readlineSync = require('readline-sync');
+
+var Paths = require("../Tools/Paths");
+
+
+var FileParser = (function () 
+{
+	function FileParser() 
+	{
+		this.BangSetups = [];
+		this.PreviousTrigger ="";
+		this.PreviousCommand = "";
+		this.CustomVariables = {};
+	}
+
+	FileParser.prototype.ProcessFile = function (filePath)
+	{
+		var data = fs.readFileSync(filePath).toString().trim();
+		
+		console.log(chalk.yellow(util.format("\nProcessing %s", filePath)));
+
+		var commandModule = this.ProcessData(data, path.basename(filePath));
+		
+		if(this.BangSetups.length)
+		{
+			var self = this;
+			this.BangSetups.forEach(function(setup)
+			{
+				console.log(chalk.yellow(util.format("\nTo use the \"!%s\" command you will need to also install the following module into your map: %s", setup.bangName, setup.fileName)));
+				var supportModule = self.ProcessData(setup.setupData, setup.fileName);
+				self.PrintCompiledModuleSize(commandModule);
+				self.OutputCompiledModule(supportModule, false);
+				BangCommandHelper.AddSupportModuleToCache(setup);
+			});
+		}
+
+		console.log(chalk.yellow(util.format("\nProcessing of %s is complete.", commandModule.SourceName)));
+		this.PrintCompiledModuleSize(commandModule);
+		this.OutputCompiledModule(commandModule, true);
+	};
+
+	FileParser.prototype.PrintCompiledModuleSize = function(commandModule)
+	{
+		var length = commandModule.CompiledCommand.length;
+		var percentage = Math.round(((length / 32500) * 10000), 2) / 100;
+		console.log(chalk.yellow(util.format("Command length: %s (%s%)", length.toLocaleString(), percentage)));
+	}
+
+	FileParser.prototype.OutputCompiledModule = function(commandModule, isLast)
+	{	
+		if(Settings.Current.Output.WriteCompiledCommandsToFile)
+		{
+			var outputFileName = path.resolve(Paths.LocalDirectory + "/" + commandModule.SourceName.replace(".mcc", ".oc"));
+			fs.writeFileSync(outputFileName, commandModule.CompiledCommand);
+			console.log(chalk.green("\n * The compiled command has been saved to " + outputFileName));
+		}
+
+		if(Settings.Current.Output.ShowCompiledCommands)
+		{
+			console.log(chalk.green("\n\ * COMPILED-COMMAND:\n"));
+			console.log(commandModule.CompiledCommand);
+		}
+
+		if(Settings.Current.Output.CopyCompiledCommands)
+		{
+			// Copy to the clipboard
+			ncp.copy(commandModule.CompiledCommand);
+			console.log(chalk.green("\n * The compiled command has been copied into your clipboard."));
+
+			// Give the user time to use the clipboard before moving on.
+			if(!isLast) readlineSync.keyIn(chalk.green("   Install into your world before you continue. Type 'c' to continue. "), {limit: 'c'});
+		}
+	};
+	
+	FileParser.prototype.ProcessData = function (data, sourceName)
+	{
+		// Create a new module instance
+		var commandModule = new CommandModule();
+		commandModule.SourceName = sourceName;
+
+		commandModule.setCoordinates(
+			Settings.Current.Modules.StartX,
+			Settings.Current.Modules.StartY,
+			Settings.Current.Modules.StartZ,
+			Settings.Current.Modules.StopX,
+			Settings.Current.Modules.StopY,
+			Settings.Current.Modules.StopZ,
+			Settings.Current.Modules.Border
+		);
+
+		// Pass module to CommandCreator to start new vars
+		CommandCreator.reset(commandModule);
+
+		var content = this.removeComments(data.toString().trim());
+		var lines = content.split("\n");
+		var distanceOffset = 3;
+		
+		var type = "impulse";
+		var conditional = false;
+		var auto = true;
+		
+		var commands = [];
+
+		var summonModuleDisplayMarker = CommandCreator.addNewModuleDisplayMarker(path.basename(sourceName));
+
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log(chalk.bold("\n\n* START NEW FILE!"))
+			console.log("  " + sourceName);
+			if(summonModuleDisplayMarker) console.log("   -> " + summonModuleDisplayMarker);
+			else console.log("   -> " + "No file marker summoned");
+		}
+		
+		var self = this;
+		var process = function(line, endoffile)
+		{
+			try
+			{
+				self.processLine(commandModule, line, endoffile);
+			}
+			catch(err)
+			{
+				console.log(chalk.red.bold("\n\n  LINE ERROR!"));
+				console.log(util.format(chalk.red.bold("  Error on %s:%d - %s\n\n"), sourceName, i, err));
+				throw err;
+			}
+		}
+
+		// Loop through each line in the file.
+		for(i=0; i < lines.length; i++)
+		{
+			var line = lines[i].trim();
+			process(line, false);
+		}
+
+		// One final call to processLine to complete the last trigger
+		process("", true);
+
+		
+
+		var summonRebuildEntityCommand = util.format(
+				Templates.Current.SUMMON_REBUILD_ENTITY,
+				commandModule.lowX,
+				(commandModule.lowY - 1), // lower y by 1 because minecarts execute 1 block up
+				commandModule.lowZ
+			);
+
+		var clearAreacommand = util.format(
+				Templates.Current.CLEAR_AREA_FORMAT, 
+				commandModule.border,
+				commandModule.lowY-1,
+				commandModule.border,
+				(commandModule.diffX - commandModule.border),
+				commandModule.diffY,
+				(commandModule.diffZ - commandModule.border)
+			);
+
+		var clearMarkersCommand = util.format(
+				Templates.Current.CLEAR_MARKERS_FORMAT,
+				commandModule.diffX,
+				commandModule.diffY,
+				commandModule.diffZ
+			);
+
+		// Add commands to start of combined command
+		commandModule.Commands.unshift(
+			Templates.Current.CLEAR_MODULE_DISPLAY_MARKER,
+			summonRebuildEntityCommand, 
+			clearAreacommand, 
+			clearMarkersCommand,
+			summonModuleDisplayMarker
+		);
+		
+		
+		var removeBlocksNextTickCommand = CommandCreator.buildSetblockCommand(0, 2, 0, "up", "impulse", false, true, false, "", "/fill ~ ~-1 ~ ~ ~ ~ air");
+		
+		// Add commands to end of combined command
+		commandModule.Commands.push(
+			removeBlocksNextTickCommand, 
+			Templates.Current.CLEAR_REBUILD_ENTITY,
+			Templates.Current.CLEAR_MINECARTS
+		);
+
+		// Now take all in this.Commands and put into commandblock minecarts to be executed
+		// when summoned as passengers on an activator rail
+		
+		var minecarts = []
+		for(i=0; i < commandModule.Commands.length; i++)
+		{
+			var command = commandModule.Commands[i];
+			var minecart = util.format(Templates.Current.COMMAND_BLOCK_MINECART_NBT_FORMAT, JSON.stringify(command)); 
+			minecarts.push(minecart);
+		}
+		
+		var minecartsString = minecarts.join(",");
+
+		var compiledCommands = util.format(Templates.Current.SUMMON_FALLING_RAIL_FORMAT, minecartsString);
+
+		commandModule.CompiledCommand = compiledCommands;
+
+		return commandModule;
+	};
+
+	FileParser.prototype.removeComments = function(content)
+	{
+		var blockComments = String.raw`\/\*(.|[\r\n])*?\*\/`;
+		var lineComments = String.raw`\/\/(.*?)\r?($|\n)`;
+		var strings = String.raw`"((\\[^\n]|[^"\n])*)"`;
+
+		var expression = new RegExp(blockComments + "|" + lineComments + "|" + strings, 'g');
+
+		content = content.replace(expression, function(match, offset, str)
+		{
+			if(match.startsWith("//"))
+			{
+				// It's a line comment, replace with new line.
+				return "\n"
+			}
+			else if(match.startsWith("/*"))
+			{
+				// It's a block comment, remove it all.
+				return '';
+			}
+			else if(match[0] == `"`)
+			{
+				// It a string; keep it.
+				return match;
+			}
+			else
+			{
+				// It's none of the above. Keep just in case.
+				return match;
+			}
+		});
+
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log(chalk.bold("\n\n* REMOVING COMMENTS!"))
+			console.log(chalk.bold("\n\n  RESULT:"))
+			console.log(content);
+		}
+
+		return content.trim();
+	}
+		
+	FileParser.prototype.processLine = function (commandModule, line , endoffile)
+	{
+		if(line[0] == "#" || line[0] == ">" || line[0] == "/" || line[0] == "!" || line[0] == "$" || endoffile == true) 
+		{
+			// If a new trigger is found, or this is the endoffile, process the previous line
+			switch (this.PreviousTrigger)
+			{
+				case "#":
+					this.processRowLine(commandModule, this.PreviousCommand.trim());
+					break;
+				case ">":
+					this.processJsonLine(this.PreviousCommand.trim());
+					break;
+				case "/":
+					this.processCommandBlockLine(commandModule, this.PreviousCommand.trim());
+					break;
+				case "!":
+					this.processBangLine(commandModule, this.PreviousCommand.trim());
+					break;
+				case "$":
+					this.processVariableLine(this.PreviousCommand.trim());
+					break;
+			}
+			this.PreviousTrigger = line[0];
+			this.PreviousCommand = "";
+		}
+
+		this.PreviousCommand += " " + line;
+	};
+	
+	FileParser.prototype.processRowLine = function(commandModule, line)
+	{
+		line=this.checkForVariables(line);
+		var summon = CommandCreator.startNewRow(line);
+		if(summon) commandModule.addCommand(summon);
+		
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log(chalk.bold("\n\n* START NEW LINE!"))
+			console.log("  " + line);
+			if(summon) console.log("   -> " + summon);
+			else console.log("   -> " + "No line marker summoned");
+		}
+	};
+	
+	FileParser.prototype.processJsonLine = function(line)
+	{
+		line=this.checkForVariables(line);
+
+		var json = JSON.parse(line.replace(">",""));
+		
+
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log(chalk.bold("\n* PROCESS JSON OPTIONS"));
+			console.log("  " + JSON.stringify(json));
+			console.log("   -> type = " + CommandCreator.currentCommandBlock.type);
+			console.log("   -> conditional = " + CommandCreator.currentCommandBlock.conditional);
+			console.log("   -> auto = " + CommandCreator.currentCommandBlock.auto);
+			console.log("   -> trackOutput = " + CommandCreator.currentCommandBlock.trackOutput);
+			console.log("   -> executeAs = " + CommandCreator.executeAs);
+			console.log("   -> markerTag = " + CommandCreator.markerTag);
+		}
+
+		CommandCreator.processJSONLine(json);
+	};
+	
+	FileParser.prototype.processCommandBlockLine = function(commandModule, line)
+	{
+		// Replace TABS
+		line=line.replace(/\t/g,' ');
+		// Remove starting /
+		line=line.substr(1);
+		// Replace variables
+		line=this.checkForVariables(line);
+
+		var cornerCommands = CommandCreator.fixConditionalCorners();
+		if(cornerCommands.length > 0)
+		{
+			if(Settings.Current.Output.ShowDebugInfo)
+				console.log(chalk.bold("\n* CONDITIONAL CORNER FIX:"));
+			for(var i in cornerCommands)
+			{	
+				var cornerCmd = cornerCommands[i];
+				commandModule.addCommand(cornerCmd);
+				if(Settings.Current.Output.ShowDebugInfo)
+					console.log("   -> " + cornerCmd);
+			}
+		}
+
+		var summon = CommandCreator.addNewCmdMarker();
+		if(summon) commandModule.addCommand(summon);
+		
+		var command = CommandCreator.addSetblockCommand(line);
+		commandModule.addCommand(command);
+		
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+				console.log(chalk.bold("\n* CREATE COMMAND BLOCK"));
+				console.log("  " + line);
+				console.log("   -> " + command);
+				if(summon) console.log("   -> " + summon);
+		}
+	};
+	
+	FileParser.prototype.processBangLine = function(commandModule, line)
+	{
+		line=this.checkForVariables(line);
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log(chalk.bold("\n* PROCESS BANG COMMAND"));
+			console.log("  " + line);
+		}
+		var commands = BangCommandHelper.ProcessBang(commandModule, line, this);
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log("  Commands generated:");
+		}
+		if(commands.length > 0)
+		{
+			commands.forEach(function(command)
+			{
+				if(Settings.Current.Output.ShowDebugInfo) console.log("   -> " + command);
+				commandModule.addCommand(command);
+			});
+		}
+	};
+
+	FileParser.prototype.processVariableLine = function(line)
+	{
+		// varName; everything up to the first =
+		var varName = line.substr(0,line.indexOf('=')).trim();
+		// varValue; averything after the first =
+		var varValue = line.substr(line.indexOf('=')+1).trim();
+		varValue = this.checkForVariables(varValue);
+
+		if(Settings.Current.Output.ShowDebugInfo)
+		{
+			console.log(chalk.bold("\n* VARIABLE ASSIGNED!"));
+			console.log("  '" + varName + "' = '" + varValue + "'");
+		}
+		
+		this.setVariable(varName, varValue);
+	};
+
+	FileParser.prototype.setVariable = function(varName, varValue)
+	{
+		this.CustomVariables[varName] = varValue;
+	};
+
+	FileParser.prototype.checkForVariables = function(line)
+	{
+		// Loop through the keys
+		for(var varName in this.CustomVariables)
+		{
+			var name = varName.substr(1);
+			var varNameUsed = "(\\$\\{" + name + "})|(\\$" + name + "\\b)";
+			
+			line = line.replace(new RegExp(varNameUsed, 'g'), this.CustomVariables[varName]);
+		}
+		return line;
+	};
+
+	FileParser.prototype.getVariable = function(varName)
+	{
+		return this.CustomVariables[varName];
+	}
+
+	FileParser.prototype.AddBangSetup = function(bangSetup)
+	{
+		var exists = false;
+
+		if(BangCommandHelper.IsSupportModuleInCache(bangSetup))
+			exists = true;
+		else
+		{
+			this.BangSetups.forEach(function(existingSetup)
+			{
+				if(bangSetup.setupData == existingSetup.setupData)
+					exists = true;
+			});
+		}
+
+		if(!exists)
+			this.BangSetups.push(bangSetup);
+	}
+	
+	return FileParser;
+	
+})();
+
+module.exports = FileParser;
