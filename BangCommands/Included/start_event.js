34dc4844a8013ee9137291f883e1d555a69c5565
nift4 <36458596+nift4@users.noreply.github.com>
2018-07-15 09:21:44 +0200

    1.13

--- BangCommands/Included/start_event.js
+++ BangCommands/Included/start_event.js
@@ -14,10 +14,14 @@ StartEvent.Install = Event.Install;
 StartEvent.Execute = function(smelt)
 {
 	var name = smelt.args[0];
+   var tag = "tag";
+    if (smelt.settings.Output.MinecraftVersion == "1.9" || smelt.settings.Output.MinecraftVersion == "1.10" || smelt.settings.Output.MinecraftVersion == "1.11" || smelt.settings.Output.MinecraftVersion == "1.12"){
+      tag = "scoreboard players tag"; }
+
 	if(name)
 	{
 		smelt.addCommandBlock(
-			util.format("/tag @e[tag=Smelt_SYSTEM] add event_%s", name)
+			util.format("/%s @e[tag=Smelt_SYSTEM] add event_%s",tag, name)
 		);
 	}
 };
